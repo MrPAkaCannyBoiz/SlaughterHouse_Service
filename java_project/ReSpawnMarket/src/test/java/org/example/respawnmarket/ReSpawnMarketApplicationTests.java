6588166efe35403490c9edb538cc69d2a73161c3
MrPAkaCannyBoiz <pongsatorn5762@gmail.com>
2025-10-29 11:56:46 +0100

    starting

---
+++ java_project/ReSpawnMarket/src/test/java/org/example/respawnmarket/ReSpawnMarketApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.respawnmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSpawnMarketApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
